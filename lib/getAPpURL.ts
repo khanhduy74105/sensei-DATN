049e8930d8a57d99e89dd2891bb6b0ff2cc0be93
KhanhDuy <khanhduy74105@gmail.com>
2025-12-20 12:36:00 +0700

    Update app url

--- lib/getAPpURL.ts
+++ lib/getAPpURL.ts
@@ -6,7 +6,7 @@ export const getAppUrl = () => {
   
   // Server-side: check env
   if (process.env.VERCEL_URL) {
-    return `https://${process.env.VERCEL_URL}`;
+    return `https://sensei-datn-lmae.vercel.app`;
   }
   
   return process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000';
