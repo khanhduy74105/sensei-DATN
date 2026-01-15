115abece8f61f82fc5d2b65c935f9dc4a7f805a8
KhanhDuy <khanhduy74105@gmail.com>
2026-01-15 15:09:25 +0700

    as

--- app/(common)/(main)/resume/page.tsx
+++ app/(common)/(main)/resume/page.tsx
@@ -1,11 +1,12 @@
-import { getAllResumes } from "@/actions/resume";
-import ResumeBuilderPre from "./_components/resume-builder-pre";
+import { getResume } from "@/actions/resume";
+import ResumeBuilder from "./_components/resume-builder";
 
-export default async function ResumePrePage() {
-  const resumes = await getAllResumes();
+export default async function ResumePage() {
+  const resume = await getResume();
+
   return (
     <div className="container mx-auto py-6">
-        <ResumeBuilderPre resumes={resumes}/>
+      <ResumeBuilder initialContent={resume?.content} />
     </div>
   );
 }
